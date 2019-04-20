54d5f3f4df1fdf7979c1a764a3908cec5249ba51
Dominik Skála <DominikSkala@seznam.cz>
2019-04-20 23:48:15 +0200

    more updates

--- modules/v1/controllers/ServerController.php
+++ modules/v1/controllers/ServerController.php
@@ -69,7 +69,7 @@ class ServerController extends ApiController
 			'query' => Server::find()
 				->join('LEFT JOIN', '{{statistic_players}} as sp', 'server.id = sp.server_id')->orderBy('sp.date DESC, sp.value DESC')->distinct(true)
 				->join('LEFT JOIN', '{{statistic_status}} as ss', 'server.id = ss.server_id') ->orderBy('sp.date DESC')
-				->andWhere('sp.value = 1'),
+				->orderBy('sp.value DESC'),
 			'pagination' => [
 				'defaultPageSize' => 12,
 				'pageSize' => 12, //to set count items on one page, if not set will be set from defaultPageSize
