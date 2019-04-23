c479e224e58605413b0bd478b167b54900e6c22c
Dominik Skála <DominikSkala@seznam.cz>
2019-04-23 18:35:46 +0200

    migration fix

--- migrations/m190423_105005_add_image_url.php
+++ migrations/m190423_105005_add_image_url.php
@@ -12,8 +12,8 @@ class m190423_105005_add_image_url extends Migration
      */
     public function safeUp()
     {
-    	$this->addColumn('{{server}}', 'image_url', $this->string());
-    	$this->addColumn('{{server}}', 'thumbnail_image_url', $this->string());
+    	$this->addColumn('{{service}}', 'image_url', $this->string());
+    	$this->addColumn('{{service}}', 'thumbnail_image_url', $this->string());
 	}
 
     /**
@@ -21,7 +21,7 @@ class m190423_105005_add_image_url extends Migration
      */
     public function safeDown()
     {
-		$this->dropColumn('{{server}}', 'image_url');
-	    $this->dropColumn('{{server}}', 'background_image_url');
+		$this->dropColumn('{{service}}', 'image_url');
+	    $this->dropColumn('{{service}}', 'background_image_url');
     }
 }
