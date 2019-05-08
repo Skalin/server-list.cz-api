3edd386bbf301bfd7ca1882e93546ebd72573fbc
Dominik Skála <DominikSkala@seznam.cz>
2019-05-08 19:25:30 +0200

    updated server filtering

--- modules/v1/controllers/ServerController.php
+++ modules/v1/controllers/ServerController.php
@@ -72,7 +72,7 @@ class ServerController extends ApiController
 				JOIN (SELECT *
 				FROM `statistic_players`
 					WHERE `statistic_players`.`id` IN (
-						SELECT MAX(`statistic_players`.`id`)
+						SELECT MAX(`t`.`id`)
 						FROM `statistic_players`
 						GROUP BY `server_id`
 					)
