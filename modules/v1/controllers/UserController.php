694caac36b78bea13a0df71e9d1b580fe4b37bab
Dominik Skála <DominikSkala@seznam.cz>
2020-03-21 19:38:24 +0100

    updated controller

--- modules/v1/controllers/UserController.php
+++ modules/v1/controllers/UserController.php
@@ -19,225 +19,225 @@ use Firebase\JWT\JWT;
 use yii\data\ActiveDataProvider;
 use yii\filters\Cors;
 use yii\web\Response;
+use yii\filters\VerbFilter;
 
 class UserController extends ApiController
 {
 
-	public $modelClass = 'app\models\User';
+    public $modelClass = 'app\models\User';
 
 
-	public function behaviors()
-	{
+    public function behaviors()
+    {
 
-		$behaviors = parent::behaviors();
-		$auth = $behaviors['authenticator'];
-		unset($behaviors['authenticator']);
+        $behaviors = parent::behaviors();
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
 
-		// add CORS filter
-		$behaviors['corsFilter'] = [
-			'class' => Cors::className(),
-			'cors'  => [
-				// restrict access to domains:
-				'Origin' => static::allowedDomains(),
-				'Access-Control-Request-Method' => ['POST', 'PUT', 'OPTIONS'],
-				'Access-Control-Allow-Credentials' => true,
-				'Access-Control-Request-Headers' => ['x-requested-with', 'content-type'],
-				'Access-Control-Max-Age' => 0, // Cache (seconds)
-			],
-		];
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+            'cors' => [
+                // restrict access to domains:
+                'Origin' => static::allowedDomains(),
+                'Access-Control-Request-Method' => ['POST', 'PUT', 'OPTIONS'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Request-Headers' => ['x-requested-with', 'content-type'],
+                'Access-Control-Max-Age' => 0, // Cache (seconds)
+            ],
+        ];
 
-		$behaviors['contentNegotiator'] = [
-			'class' => 'yii\filters\ContentNegotiator',
-			'formats' => [
-				'application/json' => Response::FORMAT_JSON,
-			]
-		];
-		// re-add authentication filter
-		$behaviors['authenticator'] = $auth;
-		// avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
-		$behaviors['authenticator']['except'] = ['options'];
+        $behaviors['verbs'] = [
+            'class' => VerbFilter::className(),
+            'actions' => [
+                'update' => ['POST'],
+                'view' => ['POST'],
+            ],
+        ];
+        $behaviors['contentNegotiator'] = [
+            'class' => 'yii\filters\ContentNegotiator',
+            'formats' => [
+                'application/json' => Response::FORMAT_JSON,
+            ]
+        ];
+        // re-add authentication filter
+        $behaviors['authenticator'] = $auth;
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options'];
 
-		return $behaviors;
-	}
+        return $behaviors;
+    }
 
-	public function actions()
-	{
-		$actions = parent::actions();
-		unset($actions['create']);
-		unset($actions['update']);
-		unset($actions['index']);
-		unset($actions['view']);
-		return $actions;
-	}
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['create']);
+        unset($actions['update']);
+        unset($actions['index']);
+        unset($actions['view']);
+        return $actions;
+    }
 
-	public function actionRegister()
-	{
-		$user = new User();
-		$user->attributes = \Yii::$app->request->post('user');
+    public function actionRegister()
+    {
+        $user = new User();
+        $user->attributes = \Yii::$app->request->post('user');
 
-		if ($user->validate())
-		{
-			$user->save();
-			return $user->generateLoginToken();
-		}
-		else
-		{
-			throw new ApiException(400, $user->errors);
-		}
-	}
+        if ($user->validate()) {
+            $user->save();
+            return $user->generateLoginToken();
+        } else {
+            throw new ApiException(400, $user->errors);
+        }
+    }
 
-	public function actionLogin()
-	{
-		$data = \Yii::$app->request->post('user');
-		$user = User::findByUsername($data['username']);
-		if (!$user)
-		{
-			throw new ApiException(401, 'Username not found');
-		}
+    public function actionLogin()
+    {
+        $data = \Yii::$app->request->post('user');
+        $user = User::findByUsername($data['username']);
+        if (!$user) {
+            throw new ApiException(401, 'Username not found');
+        }
 
-		if (!$user->validatePassword($data['password']))
-		{
-			throw new ApiException(401, 'Incorrect username or password.');
-		}
+        if (!$user->validatePassword($data['password'])) {
+            throw new ApiException(401, 'Incorrect username or password.');
+        }
 
-		$loginToken = new LoginToken();
-		$loginToken->user_id = $user->id;
-		if ($loginToken->save())
-			return $loginToken->getAsJWTToken();
-		throw new ApiException(401, 'Couldn\'t generate login token.');
-	}
-
-
-	public function actionServer($id)
-	{
-
-		$user = $this->validateUser('Server');
-		if (!$user)
-			throw new ApiException(401, 'User not authorized.');
-
-		$criteria = ['id' => $id];
-		$server = Server::findOne($criteria);
-		if (!$server)
-			throw new ApiException(404, 'Server not found!');
-
-		$server = Server::findOne(array_merge($criteria, ['user_id' => $user]));
-		if (!$server)
-			throw new ApiException(403, 'Not users server.');
-
-		return true;
-	}
-
-	public function actionServers()
-	{
-		$user = $this->validateUser('Server');
-		if (!$user)
-			throw new ApiException(401, 'User not authorized.');
-
-		return Server::findAll(['user_id' => $user]);
-	}
+        $loginToken = new LoginToken();
+        $loginToken->user_id = $user->id;
+        if ($loginToken->save())
+            return $loginToken->getAsJWTToken();
+        throw new ApiException(401, 'Couldn\'t generate login token.');
+    }
 
 
+    public function actionServer($id)
+    {
 
-	public function actionLogout()
-	{
-		$user = $this->validateUser('User');
-		if (!$user)
-			throw new ApiException(401, 'User not authorized');
+        $user = $this->validateUser('Server');
+        if (!$user)
+            throw new ApiException(401, 'User not authorized.');
 
-		$this->logout();
+        $criteria = ['id' => $id];
+        $server = Server::findOne($criteria);
+        if (!$server)
+            throw new ApiException(404, 'Server not found!');
 
-		return true;
-	}
+        $server = Server::findOne(array_merge($criteria, ['user_id' => $user]));
+        if (!$server)
+            throw new ApiException(403, 'Not users server.');
 
+        return true;
+    }
 
-	private function logout()
-	{
-		$token = JWT::decode($this->getValidationData(), LoginToken::LOGIN_TOKEN_KEY, array("HS256"));
+    public function actionServers()
+    {
+        $user = $this->validateUser('Server');
+        if (!$user)
+            throw new ApiException(401, 'User not authorized.');
 
-		$model = User::findAccessToken($this->getValidationMethod(), $token->token);
-		if ($model->isExpired())
-			throw new ApiException(403, 'Token expired');
-
-		if (!$model->expire())
-			throw new ApiException(500, 'Something went wrong at saving tokens');
-	}
-
-	public function actionRelogin()
-	{
-		$user = $this->validateUser('Server');
-		if (!$user)
-			throw new ApiException(401, 'User not authorized.');
-
-		// log out user -> expire the token
-		$this->logout();
-
-		$loginToken = new LoginToken();
-		$loginToken->user_id = $user;
-		if ($loginToken->save())
-			return $loginToken->getAsJWTToken();
-		throw new ApiException(401, 'Couldn\'t generate login token.');
-	}
-
-	public function actionLogoutall()
-	{
-
-		if ($this->getValidationMethod() != User::WEB_LOGIN)
-			throw new ApiException(405, 'Not allowed from API requests');
-
-		$user = $this->validateUser('User');
-		if (!$user)
-			throw new ApiException(401, 'User not authorized');
-
-		$tokens = LoginToken::find()->user($user)->active()->all();
-
-		foreach ($tokens as $token)
-		{
-			$token->expire();
-		}
-		return true;
-	}
-
-	public function actionNotifications()
-	{
-		$user = $this->validateUser('UserNotification');
-		if (!$user)
-			throw new ApiException(401, 'User not authorized.');
+        return Server::findAll(['user_id' => $user]);
+    }
 
 
-		$dataProvider = new ActiveDataProvider([
-			'query' => UserNotification::find()
-				->user($user)
-				->orderBy('date DESC'),
-			'pagination' => [
-				'defaultPageSize' => 5,
-				'pageSize' => 5, //to set count items on one page, if not set will be set from defaultPageSize
-			]
-		]);
+    public function actionLogout()
+    {
+        $user = $this->validateUser('User');
+        if (!$user)
+            throw new ApiException(401, 'User not authorized');
+
+        $this->logout();
+
+        return true;
+    }
 
 
+    private function logout()
+    {
+        $token = JWT::decode($this->getValidationData(), LoginToken::LOGIN_TOKEN_KEY, array("HS256"));
 
-		return $dataProvider;
-	}
+        $model = User::findAccessToken($this->getValidationMethod(), $token->token);
+        if ($model->isExpired())
+            throw new ApiException(403, 'Token expired');
 
-	public function actionNotification($id)
-	{
+        if (!$model->expire())
+            throw new ApiException(500, 'Something went wrong at saving tokens');
+    }
 
-		$user = $this->validateUser('UserNotification');
-		if (!$user)
-			throw new ApiException(401, 'User not authorized.');
+    public function actionRelogin()
+    {
+        $user = $this->validateUser('Server');
+        if (!$user)
+            throw new ApiException(401, 'User not authorized.');
+
+        // log out user -> expire the token
+        $this->logout();
+
+        $loginToken = new LoginToken();
+        $loginToken->user_id = $user;
+        if ($loginToken->save())
+            return $loginToken->getAsJWTToken();
+        throw new ApiException(401, 'Couldn\'t generate login token.');
+    }
+
+    public function actionLogoutall()
+    {
+
+        if ($this->getValidationMethod() != User::WEB_LOGIN)
+            throw new ApiException(405, 'Not allowed from API requests');
+
+        $user = $this->validateUser('User');
+        if (!$user)
+            throw new ApiException(401, 'User not authorized');
+
+        $tokens = LoginToken::find()->user($user)->active()->all();
+
+        foreach ($tokens as $token) {
+            $token->expire();
+        }
+        return true;
+    }
+
+    public function actionNotifications()
+    {
+        $user = $this->validateUser('UserNotification');
+        if (!$user)
+            throw new ApiException(401, 'User not authorized.');
 
 
-		$notification = UserNotification::find()->user($user)->andWhere(['id' => $id])->one();
-		if (!$notification)
-			throw new ApiException(403, 'Not your notification.');
+        $dataProvider = new ActiveDataProvider([
+            'query' => UserNotification::find()
+                ->user($user)
+                ->orderBy('date DESC'),
+            'pagination' => [
+                'defaultPageSize' => 5,
+                'pageSize' => 5, //to set count items on one page, if not set will be set from defaultPageSize
+            ]
+        ]);
 
-		$notification->read = 1;
-		if ($notification->save())
-			return $notification;
-		return $notification->errors;
-	}
 
-	public function actionUpdate()
+        return $dataProvider;
+    }
+
+    public function actionNotification($id)
+    {
+
+        $user = $this->validateUser('UserNotification');
+        if (!$user)
+            throw new ApiException(401, 'User not authorized.');
+
+
+        $notification = UserNotification::find()->user($user)->andWhere(['id' => $id])->one();
+        if (!$notification)
+            throw new ApiException(403, 'Not your notification.');
+
+        $notification->read = 1;
+        if ($notification->save())
+            return $notification;
+        return $notification->errors;
+    }
+
+    public function actionUpdate()
     {
 
         $user = $this->validateUser('Server');
@@ -246,8 +246,7 @@ class UserController extends ApiController
 
         $user = User::findById($user);
         $user->attributes = \Yii::$app->request->post('user');
-        if (!$user->validate())
-        {
+        if (!$user->validate()) {
             throw new ApiException(400, $user->errors);
         }
 
